39bb4dff5e8a24d3e2060dff4e02b3d2d24c8ee6
Henry Salazar <henrysteven003@gmail.com>
2018-10-01 21:06:40 -0500

    profile linked

--- DonLimpioApp/app/src/main/java/javeriana/edu/co/donlimpio/ScheduleActivity.java
+++ DonLimpioApp/app/src/main/java/javeriana/edu/co/donlimpio/ScheduleActivity.java
@@ -1,13 +1,30 @@
 package javeriana.edu.co.donlimpio;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ScheduleActivity extends AppCompatActivity {
 
+    private Button mBtnDo;
+    private TextView mCancel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
+        mBtnDo = (Button) findViewById(R.id.btnDo);
+        mCancel = (TextView) findViewById(R.id.tvcancel);
+
+        mCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), ServicesActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- DonLimpioApp/app/src/main/java/javeriana/edu/co/donlimpio/ServicesActivity.java
+++ DonLimpioApp/app/src/main/java/javeriana/edu/co/donlimpio/ServicesActivity.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class ServicesActivity extends AppCompatActivity {
 
     TextView mLimpieza;
+    ImageButton btnProfile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +22,15 @@ public class ServicesActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(getApplicationContext(), LocationActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btnProfile = (ImageButton) findViewById(R.id.profile);
+        btnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), ProfileActivity.class);
                 startActivity(i);
             }
         });
