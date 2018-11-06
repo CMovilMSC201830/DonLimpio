b5629e2a3add7454c97667e37f55eacb2bb4f0ee
ingquimbay <quimbayc@javeriana.edu.co>
2018-11-06 03:09:12 -0500

    Select Image from camera and gallery

--- DonLimpioApp/app/src/main/java/javeriana/edu/co/donlimpio/ProfileActivity.java
+++ DonLimpioApp/app/src/main/java/javeriana/edu/co/donlimpio/ProfileActivity.java
@@ -1,19 +1,62 @@
 package javeriana.edu.co.donlimpio;
 
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.InputStream;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private String TAG = "EditProfile";
+    public static final int IMAGE_REQUEST = 100;
+    public static final int CAMERA_REQUEST = 20;
+    private StorageReference mStorageRef, imageRef;
+    CircleImageView circlePhoto;
+    //ImageButton circlePhoto;
+    Uri imageUri, downloadPhoto;
+    InputStream imageStream;
+    UploadTask uploadTask;
     Button ownS;
+    View profileLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        profileLayout = findViewById(R.id.profile_layout);
+
         ownS = (Button) findViewById(R.id.own_services);
+        circlePhoto = (CircleImageView) findViewById(R.id.profile_image);
+        //circlePhoto = (ImageButton) findViewById(R.id.profile_image);
+
         ownS.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -21,5 +64,135 @@ public class ProfileActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        boolean hasPermissionGallery = (ContextCompat.checkSelfPermission(getApplicationContext(),
+                android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
+        if (!hasPermissionGallery)
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE},IMAGE_REQUEST);
+        else {
+            circlePhoto.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    choosePhoto();
+                }
+            });
+        }
+
+        boolean hasPermissionCamera = (ContextCompat.checkSelfPermission(getApplicationContext(),
+                android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED);
+        if (!hasPermissionCamera) {
+            ActivityCompat.requestPermissions(this,new String[]{android.Manifest.permission.CAMERA},CAMERA_REQUEST);
+        } else {
+            circlePhoto.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    choosePhoto();
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case IMAGE_REQUEST: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Snackbar.make(profileLayout, "¡Permiso concedido!", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                } else {
+                    Snackbar.make(profileLayout, "¡La aplicación no puede usar la galeria!",
+                            Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                }
+            }
+            case CAMERA_REQUEST: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Snackbar.make(profileLayout, "¡Permiso concedido!", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                } else {
+                    Snackbar.make(profileLayout, "¡La aplicación no puede usar la cámara!",
+                            Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                }
+            }
+        }
+    }
+
+    private void choosePhoto() {
+        final CharSequence[] options = {"Camara", "Galeria", "Cancelar"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(ProfileActivity.this);
+        builder.setTitle("Cambiar Foto");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (options[i].equals("Camara")) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    File f = new File(android.os.Environment.getExternalStorageDirectory(), "fotoPerfil.jpg");
+                    imageUri = Uri.fromFile(f);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                    startActivityForResult(intent, CAMERA_REQUEST);
+                } else if (options[i].equals("Galeria")) {
+                    Intent intent = new Intent(Intent.ACTION_PICK);
+                    intent.setType("image/*");
+                    startActivityForResult(intent, IMAGE_REQUEST);
+                } else if (options[i].equals("Cancelar")) {
+                    dialogInterface.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case CAMERA_REQUEST: {
+                if (resultCode == RESULT_OK) {
+                    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                    BitmapFactory.decodeFile(imageUri.getPath()).compress(Bitmap.CompressFormat.JPEG, 80, bytes);
+                    circlePhoto.setImageBitmap(BitmapFactory.decodeStream(new ByteArrayInputStream(bytes.toByteArray())));
+                }
+            }
+            case IMAGE_REQUEST: {
+                if (resultCode == RESULT_OK) {
+                    try{
+                        imageUri = data.getData();
+                        circlePhoto.setImageBitmap(BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri)));
+                    }catch (Exception e){
+                        Log.d(TAG, e.getMessage());
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    private void uploadFireBase(){
+        uploadTask = imageRef.putFile(imageUri);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Snackbar.make(profileLayout, "¡Hubo error subiendo la foto!",
+                        Snackbar.LENGTH_LONG).setAction("Action", null).show();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(getBaseContext(), "Foto se cargo exitosamente", Toast.LENGTH_LONG).show();
+                //downloadPhoto = taskSnapshot.getDownloadUrl();
+            }
+        });
+    }
+
+    private String getRealPathFromURI(Uri contentURI) {
+        Cursor cursor = getContentResolver().query(contentURI, null, null, null, null);
+        if (cursor == null) {
+            return contentURI.getPath();
+        } else {
+            cursor.moveToFirst();
+            int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+            return cursor.getString(idx);
+        }
     }
 }
--- DonLimpioApp/app/src/main/java/javeriana/edu/co/donlimpio/SignupActivity.java
+++ DonLimpioApp/app/src/main/java/javeriana/edu/co/donlimpio/SignupActivity.java
@@ -1,8 +1,18 @@
 package javeriana.edu.co.donlimpio;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
@@ -10,6 +20,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -19,161 +30,274 @@ import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 import javeriana.edu.co.classes.User;
 
 public class SignupActivity extends AppCompatActivity {
 
-  private static final String TAG_SIGNIN = "EMAIL_SIGNIN";
-  private static final String TAG = "AddToDatabase";
-  private EditText mName;
-  private EditText mLastName;
-  private EditText mEmail;
-  private EditText mPsswd;
-  private EditText mRepeatPsswd;
-  private EditText mPhone;
-  private Button mSignupBtn;
+    private static final String TAG_SIGNIN = "EMAIL_SIGNIN";
+    private static final String TAG = "AddToDatabase";
+    private static final String TAG_PROFILE = "CreateProfile";
+    public static final int IMAGE_REQUEST = 100;
+    public static final int CAMERA_REQUEST = 20;
+    private EditText mName;
+    private EditText mLastName;
+    private EditText mEmail;
+    private EditText mPsswd;
+    private EditText mRepeatPsswd;
+    private EditText mPhone;
+    private Button mSignupBtn;
+    View signupLayout;
+    CircleImageView circlePhoto;
+    Uri imageUri;
 
-  FirebaseAuth mAuth;
-  FirebaseAuth.AuthStateListener mAuthListener;
-  DatabaseReference myRef;
+    FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthListener;
+    DatabaseReference myRef;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_signup);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
 
-    mAuth = FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
 
-    mName = (EditText) findViewById(R.id.input_firstname);
-    mLastName = (EditText) findViewById(R.id.input_lastname);
-    mPsswd = (EditText) findViewById(R.id.input_password);
-    mEmail = (EditText) findViewById(R.id.input_email);
-    mRepeatPsswd = (EditText) findViewById(R.id.input_reenter_password);
-    mPhone = (EditText) findViewById(R.id.input_phoneNumber);
-    mSignupBtn = (Button) findViewById(R.id.signup_btn);
+        mName = (EditText) findViewById(R.id.input_firstname);
+        mLastName = (EditText) findViewById(R.id.input_lastname);
+        mPsswd = (EditText) findViewById(R.id.input_password);
+        mEmail = (EditText) findViewById(R.id.input_email);
+        mRepeatPsswd = (EditText) findViewById(R.id.input_reenter_password);
+        mPhone = (EditText) findViewById(R.id.input_phoneNumber);
+        mSignupBtn = (Button) findViewById(R.id.signup_btn);
+        circlePhoto = (CircleImageView) findViewById(R.id.signup_image);
+        signupLayout = findViewById(R.id.signup_layout);
 
-    mAuthListener = new FirebaseAuth.AuthStateListener() {
-      @Override
-      public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-        FirebaseUser user = firebaseAuth.getCurrentUser();
-        if (user != null) {
-          // User is signed in
-          Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
-          Toast.makeText(getBaseContext(),"Successfully signed in with: " + user.getEmail(),
-                  Toast.LENGTH_LONG).show();
-        } else {
-          // User is signed out
-          Log.d(TAG, "onAuthStateChanged:signed_out");
-          Toast.makeText(getBaseContext(),"Successfully signed out.",
-                  Toast.LENGTH_LONG).show();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    Toast.makeText(getBaseContext(), "Successfully signed in with: " + user.getEmail(),
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    Toast.makeText(getBaseContext(), "Successfully signed out.",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        };
+
+        boolean hasPermissionGallery = (ContextCompat.checkSelfPermission(getApplicationContext(),
+                android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
+        if (!hasPermissionGallery)
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE}, IMAGE_REQUEST);
+        else {
+            circlePhoto.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    choosePhoto();
+                }
+            });
         }
-        // ...
-      }
-    };
 
-    mSignupBtn.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        createAccount();
+        boolean hasPermissionCamera = (ContextCompat.checkSelfPermission(getApplicationContext(),
+                android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED);
+        if (!hasPermissionCamera) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA}, CAMERA_REQUEST);
+        } else {
+            circlePhoto.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    choosePhoto();
+                }
+            });
+        }
 
-      }
-    });
+        mSignupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createAccount();
 
-  }
-
-  @Override
-  public void onStart() {
-    super.onStart();
-    mAuth.addAuthStateListener(mAuthListener);
-  }
-
-  @Override
-  public void onStop() {
-    super.onStop();
-    if (mAuthListener != null) {
-      mAuth.removeAuthStateListener(mAuthListener);
-    }
-  }
-
-
-  private void createAccount() {
-    boolean okName = isAlpha(mName.getText().toString());
-    boolean okLastName = isAlpha(mLastName.getText().toString());
-    boolean okEmail = validEmail(mEmail.getText().toString());
-    boolean okPsswd = verifyPasswds(mPsswd.getText().toString(), mRepeatPsswd.getText().toString());
-    boolean okPhone = verifyPhone(mPhone.getText().toString());
-
-    if (!okName) {
-      Toast.makeText(getApplicationContext(), R.string.name_only_letters, Toast.LENGTH_SHORT)
-          .show();
-    }
-    if (!okLastName) {
-      Toast.makeText(getApplicationContext(), R.string.lastname_only_letters, Toast.LENGTH_SHORT)
-          .show();
-    }
-    if (!okPsswd) {
-      Toast.makeText(getApplicationContext(), R.string.no_repeat_psswd, Toast.LENGTH_SHORT).show();
-    }
-    if (!okPhone) {
-      Toast.makeText(getApplicationContext(), R.string.phone_matches, Toast.LENGTH_SHORT).show();
-    }
-
-    if (okName && okLastName && okEmail && okPsswd && okPhone) {
-      addUserFirebase();
-    }
-  }
-
-  private void addUserFirebase() {
-    mAuth.createUserWithEmailAndPassword(mEmail.getText().toString(), mPsswd.getText().toString())
-        .addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
-          @Override
-          public void onComplete(@NonNull Task<AuthResult> task) {
-            if (task.isSuccessful()) {
-              Log.d(TAG_SIGNIN, "createUserWithEmail:onComplete:" + task.isSuccessful());
-              FirebaseUser user = mAuth.getCurrentUser();
-              if (user != null) { //Update user Info
-                UserProfileChangeRequest.Builder upcrb = new UserProfileChangeRequest.Builder();
-                upcrb.setDisplayName(
-                    mName.getText().toString() + " " + mLastName.getText().toString());
-                //upcrb.setPhotoUri(Uri.parse("res/to/pic"));//fake uri, use Firebase Storage
-                user.updateProfile(upcrb.build());
-                myRef = FirebaseDatabase.getInstance().getReference();
-                myRef.child("Users").child(user.getUid()).setValue(new User(mEmail.getText().toString(),mName.getText().toString(),
-                        mLastName.getText().toString(),Long.parseLong(mPhone.getText().toString())));
-                startActivity(new Intent(SignupActivity.this, ServicesActivity.class)); //o en el listener
-                finish();
-              }
             }
-            if (!task.isSuccessful()) {
-              Toast.makeText(SignupActivity.this,
-                  R.string.auth_failed + task.getException().toString(),
-                  Toast.LENGTH_SHORT).show();
-              Log.e(TAG_SIGNIN, task.getException().getMessage());
-            }
-          }
         });
-  }
 
-  private boolean validEmail(String email) {
-    return !TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email)
-        .matches();
-  }
-
-  public boolean isAlpha(String name) {
-    return name.matches("[a-zA-Z\\s]{5,10}");
-  }
-
-  private boolean verifyPhone(String phone) {
-    if (10 == phone.length()) {
-      return true;
     }
-    return false;
-  }
 
-  private boolean verifyPasswds(String password1, String password2) {
-    if (password1.equals(password2)) {
-      return true;
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
     }
-    return false;
-  }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case IMAGE_REQUEST: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Snackbar.make(signupLayout, "¡Permiso concedido!", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                } else {
+                    Snackbar.make(signupLayout, "¡La aplicación no puede usar la galeria!",
+                            Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                }
+            }
+            case CAMERA_REQUEST: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Snackbar.make(signupLayout, "¡Permiso concedido!", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                } else {
+                    Snackbar.make(signupLayout, "¡La aplicación no puede usar la cámara!",
+                            Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                }
+            }
+        }
+    }
+
+    private void choosePhoto() {
+        final CharSequence[] options = {"Camara", "Galeria", "Cancelar"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(SignupActivity.this);
+        builder.setTitle("Cambiar Foto");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (options[i].equals("Camara")) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    File f = new File(android.os.Environment.getExternalStorageDirectory(), "fotoPerfil.jpg");
+                    imageUri = Uri.fromFile(f);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                    startActivityForResult(intent, CAMERA_REQUEST);
+                } else if (options[i].equals("Galeria")) {
+                    Intent intent = new Intent(Intent.ACTION_PICK);
+                    intent.setType("image/*");
+                    startActivityForResult(intent, IMAGE_REQUEST);
+                } else if (options[i].equals("Cancelar")) {
+                    dialogInterface.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case CAMERA_REQUEST: {
+                if (resultCode == RESULT_OK) {
+                    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                    BitmapFactory.decodeFile(imageUri.getPath()).compress(Bitmap.CompressFormat.JPEG, 80, bytes);
+                    circlePhoto.setImageBitmap(BitmapFactory.decodeStream(new ByteArrayInputStream(bytes.toByteArray())));
+                }
+            }
+            case IMAGE_REQUEST: {
+                if (resultCode == RESULT_OK) {
+                    try{
+                        imageUri = data.getData();
+                        circlePhoto.setImageBitmap(BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri)));
+                    }catch (Exception e){
+                        Log.d(TAG, e.getMessage());
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    private void createAccount() {
+        boolean okName = isAlpha(mName.getText().toString());
+        boolean okLastName = isAlpha(mLastName.getText().toString());
+        boolean okEmail = validEmail(mEmail.getText().toString());
+        boolean okPsswd = verifyPasswds(mPsswd.getText().toString(), mRepeatPsswd.getText().toString());
+        boolean okPhone = verifyPhone(mPhone.getText().toString());
+
+        if (!okName) {
+            Toast.makeText(getApplicationContext(), R.string.name_only_letters, Toast.LENGTH_SHORT)
+                    .show();
+        }
+        if (!okLastName) {
+            Toast.makeText(getApplicationContext(), R.string.lastname_only_letters, Toast.LENGTH_SHORT)
+                    .show();
+        }
+        if (!okPsswd) {
+            Toast.makeText(getApplicationContext(), R.string.no_repeat_psswd, Toast.LENGTH_SHORT).show();
+        }
+        if (!okPhone) {
+            Toast.makeText(getApplicationContext(), R.string.phone_matches, Toast.LENGTH_SHORT).show();
+        }
+
+        if (okName && okLastName && okEmail && okPsswd && okPhone) {
+            addUserFirebase();
+        }
+    }
+
+    private void addUserFirebase() {
+        mAuth.createUserWithEmailAndPassword(mEmail.getText().toString(), mPsswd.getText().toString())
+                .addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG_SIGNIN, "createUserWithEmail:onComplete:" + task.isSuccessful());
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            if (user != null) { //Update user Info
+                                UserProfileChangeRequest.Builder upcrb = new UserProfileChangeRequest.Builder();
+                                upcrb.setDisplayName(
+                                        mName.getText().toString() + " " + mLastName.getText().toString());
+                                //upcrb.setPhotoUri(Uri.parse("res/to/pic"));//fake uri, use Firebase Storage
+                                user.updateProfile(upcrb.build());
+                                myRef = FirebaseDatabase.getInstance().getReference();
+                                myRef.child("Users").child(user.getUid()).setValue(new User(mEmail.getText().toString(), mName.getText().toString(),
+                                        mLastName.getText().toString(), Long.parseLong(mPhone.getText().toString())));
+                                startActivity(new Intent(SignupActivity.this, ServicesActivity.class)); //o en el listener
+                                finish();
+                            }
+                        }
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(SignupActivity.this,
+                                    R.string.auth_failed + task.getException().toString(),
+                                    Toast.LENGTH_SHORT).show();
+                            Log.e(TAG_SIGNIN, task.getException().getMessage());
+                        }
+                    }
+                });
+    }
+
+    private boolean validEmail(String email) {
+        return !TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email)
+                .matches();
+    }
+
+    public boolean isAlpha(String name) {
+        return name.matches("[a-zA-Z\\s]{5,10}");
+    }
+
+    private boolean verifyPhone(String phone) {
+        if (10 == phone.length()) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean verifyPasswds(String password1, String password2) {
+        if (password1.equals(password2)) {
+            return true;
+        }
+        return false;
+    }
 }
