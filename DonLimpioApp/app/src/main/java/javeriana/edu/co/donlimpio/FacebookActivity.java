57758e11fb172744d603a90fd495a9844aabc4c3
Roger Ramirez <3719300+roger8849@users.noreply.github.com>
2018-11-10 18:13:37 -0500

    Feature/login with facebook (#2)
    * Initial push. with login with facebook.
    
    * Login with facebook working.

---
+++ DonLimpioApp/app/src/main/java/javeriana/edu/co/donlimpio/FacebookActivity.java
@@ -0,0 +1,13 @@
+package javeriana.edu.co.donlimpio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FacebookActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_facebook);
+  }
+}
